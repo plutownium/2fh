f77a6dff63bb5f9ec77ed4a3434e28639ba18b36
Roland Mackintosh <rolandmackintosh@outlook.com>
2021-08-02 18:30:19 -0700

    profile pg is done for desktop

--- src/pages/profile/Card.js
+++ src/pages/profile/Card.js
@@ -1,21 +1,36 @@
 import React from "react";
 
-function Card(headline, subtitle, paragraph) {
+import Arrow from "../../assets/icons/icons8-expand-arrow-24.png";
+import Edit from "../../assets/icons/icons8-edit-24.png";
+
+import "./Card.css";
+
+function Card({ headline, subtitle, paragraph }) {
   return (
-    <div>
-      <div>
+    <div className="profile-card gentle-black-shadow border-rounding d-flex flex-col align-items-center">
+      <div className="profile-card-width">
         <h3>{headline}</h3>
       </div>
-      <div>
-        <p>{subtitle}</p>{" "}
-        <button>
-          <img alt="arrow" />
+      <div className="profile-card-width d-flex justify-content-between">
+        <p className="big-text-blk m-0 d-flex justify-content-center align-items-center">
+          {subtitle}
+        </p>{" "}
+        <button className="invis-borders">
+          <img alt="arrow" src={Arrow} />
         </button>
       </div>
-      <br />
-      <div>
-        <p>{paragraph}</p>
-        <button>Edit</button>
+
+      <div className="profile-card-width">
+        <hr className="card-black-bar" />
+      </div>
+      <div className="profile-card-width d-flex justify-content-between">
+        <p className="big-text-blk m-0 d-flex justify-content-center align-items-center">
+          {paragraph}
+        </p>
+        <button className="invis-borders big-text-blk d-flex justify-content-center align-items-center">
+          Edit
+          <img className="m-2" src={Edit} alt="edit" />
+        </button>
       </div>
     </div>
   );
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -4,16 +4,30 @@ import Card from "./Card";
 
 import Avatar from "../../assets/avatar.png";
 
+import "../Page.css";
+
 function Profile() {
   return (
-    <div>
+    <div className="generic-grey-bg">
       <div id="profile-header-bg">
         {/* image at top  */}
-        <div>
-          <img src={Avatar} alt="pfp" />
+        <div className="profile-avatar d-flex" id="profile-avatar-imaged">
+          <img
+            className="profile-avatar"
+            id="profile-avatar-contained"
+            src={Avatar}
+            alt="pfp"
+          />
+          <div className="d-flex flex-col p-4">
+            <h3 className="pl-6 white-text">Moueer</h3>
+            <p className="pl-6 white-text">18 Lahore</p>
+          </div>
         </div>
       </div>
-      <div id="profile-matches-screen">
+      <div
+        id="profile-matches-screen"
+        className="d-flex justify-content-center flex-col align-items-center"
+      >
         <div>
           <Card
             headline={"About Us"}
