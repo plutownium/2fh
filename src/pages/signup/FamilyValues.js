486b0c6fbabfeef03463006d8eaee6b8481a61ab
Roland Mackintosh <rolandmackintosh@outlook.com>
2021-08-06 16:41:02 -0700

    debugging familyValues screen

--- src/pages/signup/FamilyValues.js
+++ src/pages/signup/FamilyValues.js
@@ -5,23 +5,27 @@ import Base from "./Base";
 function FamilyValues() {
   const [values, setValues] = useState([]);
 
-  const addValue = (value) => {
-    const currentValues = [...values];
+  const addValue = (value, currentVals) => {
+    console.log("9:", value, currentVals);
+    const currentValues = [...currentVals];
+    console.log(10, currentValues, value);
     const newValues = currentValues.push(value);
+    console.log(12, newValues);
     setValues(newValues);
     console.log(values);
   };
 
-  const valueInput = (value) => {
+  const valueInput = (value, currentVals) => {
+    console.log(currentVals);
     return (
       <div
         onClick={() => {
-          addValue(value);
+          addValue(value, currentVals);
         }}
         className={
-          values.includes(value)
-            ? "family-value-btn signup-input-height mb-2"
-            : "family-value-btn signup-input-height mb-2 selected-div"
+          currentVals.includes(value)
+            ? "family-value-btn signup-input-height mb-2 selected-div"
+            : "family-value-btn signup-input-height mb-2"
         }
       >
         <input type="radio" />
@@ -30,82 +34,20 @@ function FamilyValues() {
     );
   };
 
-  const GenerateFamilyValues = () => {
-    return (
-      <div className="d-flex flex-wrap justify-content-between">
-        {valueInput("Generosity")}
-        <div
-          onClick={() => {
-            addValue("Honesty");
-          }}
-          className={
-            values.includes("Honesty")
-              ? "family-value-btn signup-input-height mb-2"
-              : "family-value-btn signup-input-height mb-2 selected-div"
-          }
-        >
-          <input type="radio" />
-          <label>Honesty</label>
-        </div>
-        <div
-          onClick={() => {
-            addValue("Ambition");
-          }}
-          className={
-            values.includes("Ambition")
-              ? "family-value-btn signup-input-height mb-2 selected-div"
-              : "family-value-btn signup-input-height mb-2"
-          }
-        >
-          <input type="radio" />
-          <label>Ambition</label>
-        </div>
-        <div
-          onClick={() => {
-            addValue("Modesty");
-          }}
-          className={
-            values.includes("Modesty")
-              ? "family-value-btn signup-input-height selected-div"
-              : "family-value-btn signup-input-height"
-          }
-        >
-          <input type="radio" />
-          <label>Modesty</label>
-        </div>
-        <div
-          onClick={() => {
-            addValue("Teamwork");
-          }}
-          className={
-            values.includes("Teamwork")
-              ? "family-value-btn signup-input-height selected-div"
-              : "family-value-btn signup-input-height"
-          }
-        >
-          <input type="radio" />
-          <label>Teamwork</label>
-        </div>
-        <div
-          onClick={() => {
-            addValue("Saving");
-          }}
-          className={
-            values.includes("Saving")
-              ? "family-value-btn signup-input-height selected-div"
-              : "family-value-btn signup-input-height"
-          }
-        >
-          <input type="radio" />
-          <label>Saving</label>
-        </div>
-      </div>
-    );
-  };
+  const familyValues = (
+    <div className="d-flex flex-wrap justify-content-between">
+      {valueInput("Generosity", values)}
+      {valueInput("Honesty", values)}
+      {valueInput("Ambition", values)}
+      {valueInput("Modesty", values)}
+      {valueInput("Teamwork", values)}
+      {valueInput("Saving", values)}
+    </div>
+  );
 
   const title = "IDEAL PERSON";
   const question = "WHAT ARE YOUR IDEAL FAMILY VALUES?";
-  const menu = GenerateFamilyValues();
+  const menu = familyValues;
   const nextPage = 4;
   const buttonText = "NEXT";
   return (
